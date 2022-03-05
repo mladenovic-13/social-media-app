3a5df64c8f41315a77006d17a9762795726d871d
mladenovic13 <nikolanik999@gmail.com>
2022-03-05 19:38:33 +0100

    Post Component

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,39 @@
+import "./post.css";
+import { MoreVert } from "@mui/icons-material";
+
+export default function Post() {
+  return (
+    <div className="post">
+      <div className="postWrapper">
+        <div className="postTop">
+          <div className="postTopLeft">
+            <img
+              src="/assets/person/1.jpeg"
+              alt=""
+              className="postProfileImg"
+            />
+            <span className="postUsername">Nikola Mladenovic</span>
+            <span className="postDate">5 mins ago</span>
+          </div>
+          <div className="postTopRight">
+            <MoreVert />
+          </div>
+        </div>
+        <div className="postCenter">
+          <span className="postText">Hey! It is my first post guys. (:</span>
+          <img src="/assets/post/1.jpeg" alt="" className="postImg" />
+        </div>
+        <div className="postBottom">
+          <div className="postBottomLeft">
+            <img src="/assets/like.png" alt="" className="likeIcon" />
+            <img src="/assets/heart.png" alt="" className="likeIcon" />
+            <span className="postLikeCounter">32 people like it</span>
+          </div>
+          <div className="postBottomRight">
+            <span className="postCommenttext">9 comments</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
